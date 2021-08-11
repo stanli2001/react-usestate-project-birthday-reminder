4f36e134926184c757981e54f15c7d790298f3cd
list <Stan.Li@tomtom.com>
2021-08-11 10:46:38 +1000

    add restore button

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import data from "./birthdaydata";
 import BirthdayList from "./BirthdayList";
 
 function App() {
-  const [people, setPeople] = React.useState(data);
+  const [people, setPeople] = React.useState(data);  
+  const refreshPage = () => {
+    window.location.reload();
+  };
   return (
     <main>
       <section className="container">
         <h3> {people.length} birthdays today</h3>
         <BirthdayList people={people} />
-        <button         
+        <button
           onClick={() => {
             setPeople([]);
           }}
         >
           Clear All
         </button>
+        <button onClick={refreshPage}>Restore</button>
       </section>
     </main>
   );
