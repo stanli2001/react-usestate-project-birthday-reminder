67e8fca326b6a58849e040e9d2e60412d5442745
list <Stan.Li@tomtom.com>
2021-08-19 13:02:26 +1000

    changed the birthday

--- src/birthdaydata.js
+++ src/birthdaydata.js
@@ -11,7 +11,7 @@ const data = [
     id: 2,
     name: "Hester Hogan",
     age: 32,
-    DOB: "1989/8/18",
+    DOB: "1989/8/19",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
   },
@@ -27,7 +27,7 @@ const data = [
     id: 4,
     name: "Sean Walsh",
     age: 34,
-    DOB: "1987/8/18",
+    DOB: "1987/8/19",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
   },
@@ -35,7 +35,7 @@ const data = [
     id: 5,
     name: "Lola Gardner",
     age: 29,
-    DOB: "1992/8/13",
+    DOB: "1992/8/19",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
   },
